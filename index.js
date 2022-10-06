65a53c8bb61c9396c2faa0d0ae26ceaed4a808e0
rakeshkumartulas <113280390+rakeshkumartulas@users.noreply.github.com>
2022-10-06 23:33:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const args = require("minimist")(process.argv.slice(2));
+
+const http = require("http");
+const fs = require("fs");
+
+let homepage = "";
+let projectpage = "";
+let registrationpage = "";
+
+fs.readFile("home.html",(err,home)=>{
+    if (err){
+        throw err;
+    }
+    homepage = home;
+});
+
+fs.readFile("project.html",(err,project)=>{
+    if (err){
+        throw err;
+    }
+    projectpage = project;
+
+})
+
+fs.readFile("registration.html",(err,registration)=>{
+    if (err){
+        throw err;
+    }
+    registrationpage = registration;
+});
+
+http.createServer((request,response)=>{
+    let url = request.url;
+    response.writeHeader(200,{"Content-Type":"text/html"});
+    switch(url){
+        case "/project":
+            response.write(projectpage);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationpage);
+            response.end();
+            break;
+        default:
+            response.write(homepage);
+            response.end();
+            break;
+    }
+}).listen(args.port);
